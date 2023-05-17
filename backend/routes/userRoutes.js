8931d81a6180bb9b44fb3d29c10e5945b9dd7a31
ihor-vip <vip20140902@gmail.com>
2023-05-17 15:39:04 +0300

    connect to mongoDB, create model

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import {authUser} from "../controllers/userController.js";
+import {authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile} from "../controllers/userController.js";
 
+router.post('/', registerUser);
 router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
