42bb05e5a0e910bd8b2165fbb34dee330599e6e2
ihor-vip <vip20140902@gmail.com>
2023-05-18 13:38:28 +0300

    generate JWT & save Cookie

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
-import {Schema, model} from "mongoose";
+import mongoose, {Schema} from "mongoose";
+import bcrypt from 'bcryptjs';
 
 const userSchema = new Schema({
     name: {
@@ -18,6 +19,15 @@ const userSchema = new Schema({
     timestamps: true
 });
 
-const User = model('User', userSchema)
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema)
 
 export default User;
