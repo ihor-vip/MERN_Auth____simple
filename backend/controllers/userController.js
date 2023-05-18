790fd3fb370fadea2ca6a9c9792f4e61779489e8
ihor-vip <vip20140902@gmail.com>
2023-05-18 18:53:21 +0300

    auth protect middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,21 @@ import User from '../models/userModel.js'
 import generateToken from "../utils/generateToken.js";
 
 const authUser = asyncHandler(async(req, res) => {
-    res.status(200).json({message: 'Auth User'})
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email})
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid email or password');
+    }
 });
 
 const registerUser = asyncHandler(async(req, res) => {
@@ -36,7 +50,11 @@ const registerUser = asyncHandler(async(req, res) => {
 });
 
 const logoutUser = asyncHandler(async(req, res) => {
-    res.status(200).json({message: 'Logout User'})
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({message: 'User logged out'})
 });
 
 const getUserProfile = asyncHandler(async(req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
@@ -13,6 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 app.get('/', (req, res) => res.send('Server is ready'))
