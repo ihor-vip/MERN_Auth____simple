b81f2c97387f25022273bb3a38b5d20d10df83ae
ihor-vip <vip20140902@gmail.com>
2023-05-19 09:36:19 +0300

    router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import Header from "./components/Header.jsx";
-import HomeScreen from "./screens/HomeScreen.jsx";
+import { Outlet } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 
-export default function App() {
+const App = () => {
     return (
         <>
             <Header />
-            <HomeScreen />
+            <Container className='my-2'>
+                <Outlet />
+            </Container>
         </>
-    )
-}
+    );
+};
+
+export default App;
