69ad5ba2d08a0b93671fe6678dc28fa3a5ffab93
ihor-vip <vip20140902@gmail.com>
2023-05-19 15:03:22 +0300

    login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,7 @@ const authUser = asyncHandler(async(req, res) => {
             email: user.email
         });
     } else {
-        res.status(400);
+        res.status(401);
         throw new Error('Invalid email or password');
     }
 });
@@ -57,14 +57,19 @@ const logoutUser = asyncHandler(async(req, res) => {
     res.status(200).json({message: 'User logged out'})
 });
 
-const getUserProfile = asyncHandler(async(req, res) => {
-    const user = {
-        _id: req.user._id,
-        name: req.user.name,
-        email: req.user.email
-    }
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
 
-    res.status(200).json(user)
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 const updateUserProfile = asyncHandler(async(req, res) => {
