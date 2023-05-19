6a8bfec9d55f21432e6d1d1a2a6a0fe43c983d9e
ihor-vip <vip20140902@gmail.com>
2023-05-19 09:13:50 +0300

    add homescreen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
 
 export default function App() {
     return (
         <>
             <Header />
-            <h1>MERN App</h1>
+            <HomeScreen />
         </>
     )
 }
