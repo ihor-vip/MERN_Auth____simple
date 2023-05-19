db3443c3df1a3d449a687671893ee987641fc15f
ihor-vip <vip20140902@gmail.com>
2023-05-19 10:38:54 +0300

    add redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,8 @@ import {
     Route,
     RouterProvider,
 } from 'react-router-dom';
+import store from "./store.js";
+import {Provider} from "react-redux";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeScreen from './screens/HomeScreen.jsx';
 import LoginScreen from "./screens/LoginScreen.jsx";
@@ -23,7 +25,10 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
         <React.StrictMode>
             <RouterProvider router={router} />
         </React.StrictMode>
+    </Provider>
+
 );
