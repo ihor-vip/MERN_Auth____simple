d961ac51cb7ddc0452c7404ffffb291c31740afc
ihor-vip <vip20140902@gmail.com>
2023-05-20 12:04:42 +0300

    modify for production server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
 import connectDB from './config/db.js'
+import path from 'path';
 
 dotenv.config();
 connectDB();
@@ -18,6 +19,19 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 app.get('/', (req, res) => res.send('Server is ready'))
 
 app.use(notFound);
